f3658f5f18c390de2496b062bdc32ffae22a9830
doron rotem <doronrotem@gmail.com>
2013-06-19 23:52:02 +0300

    update model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,18 @@
 class Article < CouchRest::Model::Base
+
+  property :title, String
+  property :body, String
+  property :published_at, Time
+  property :author_name, String
+  property :tags, [String]
+
+  timestamps!
+
+
+  design do
+    view :by_author
+    view :by_published_at
+  end
+
+
 end
