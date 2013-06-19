035dd7c13131351a3ffc62b6018d70441467afce
doron rotem <doronrotem@gmail.com>
2013-06-19 21:50:50 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Armchair::Application.config.secret_token = 'c78a751c809a99e204012eb91158dd804b99f6bc792b886ff408ff674ab95f75737ff38609e54517ed5df35326dc0bf3e727a1d9ca0e0ee8e7cbccf119e1b0f5'
